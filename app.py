786d79258fb27abbe9bb02defe39b057799c6315
Wolf <d.wolftripp@gmail.com>
2025-02-09 19:06:55 -0800

    have index rendered properly with taks displayed

--- app.py
+++ app.py
@@ -3,6 +3,15 @@ from flask import render_template
 
 app = Flask(__name__)
 
+
+
 @app.route("/")
 def index():
-    return render_template('index.html')
+    tasks = ["Task 1", "Task 2", "Task 3"]
+    return render_template('index.html', tasks=tasks)
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
